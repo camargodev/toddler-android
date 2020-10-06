85b22308bd15d2bc37f7777f7a12930b7bebfd8c
camargodev <camargodev@gmail.com>
2020-10-06 17:33:15 -0300

    Android Project commit

---
+++ Toddler/app/src/main/java/com/ihc/toddler/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ihc.toddler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
