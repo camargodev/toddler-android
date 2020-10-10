cb8bdfa8a41f783e149f8e426cb5a6b539807d7f
camargodev <camargodev@gmail.com>
2020-10-10 14:24:39 -0300

    Reading multiple choice questions

--- Toddler/app/src/main/java/com/ihc/toddler/activity/MultipleChoiceActivity.java
+++ Toddler/app/src/main/java/com/ihc/toddler/activity/MultipleChoiceActivity.java
@@ -2,6 +2,7 @@ package com.ihc.toddler.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,15 +18,26 @@ import com.ihc.toddler.view.ExerciseView;
 import com.ihc.toddler.view.ExerciseViewFactory;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 public class MultipleChoiceActivity extends GenericExerciseActivity {
 
     private Button answerA, answerB, answerC, answerD;
 
+    private TextToSpeech textToSpeech;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         exerciseView.mapAnswers(Arrays.asList(answerA, answerB, answerC, answerD));
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR)
+                    textToSpeech.setLanguage(new Locale("pt", "BR"));
+            }
+        });
+
     }
 
     public void answerA(View view) {
@@ -39,6 +51,14 @@ public class MultipleChoiceActivity extends GenericExerciseActivity {
     public void answerC(View view) { submitAndGoToNext(3); }
 
     public void answerD(View view) { submitAndGoToNext(4); }
+
+    public void readQuestion(View view) {
+
+        String toSpeak = questionTextView.getText().toString();
+//        Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+        textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+
+    }
 
     @Override
     protected void mapLayout() {
