7f57f8177e0d9ab15c09c16d42434dccbdf93fb4
camargodev <camargodev@gmail.com>
2020-10-11 18:18:59 -0300

    New layout for content end screen

--- Toddler/app/src/main/java/com/ihc/toddler/activity/ContentEndActivity.java
+++ Toddler/app/src/main/java/com/ihc/toddler/activity/ContentEndActivity.java
@@ -1,12 +1,16 @@
 package com.ihc.toddler.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ihc.toddler.R;
 import com.ihc.toddler.manager.QuizManager;
+
+import javax.xml.datatype.Duration;
 
 public class ContentEndActivity extends AppCompatActivity {
 
@@ -16,4 +20,7 @@ public class ContentEndActivity extends AppCompatActivity {
         setContentView(R.layout.content_end_activity);
     }
 
+    public void backToMenu(View view) {
+        finish();
+    }
 }
