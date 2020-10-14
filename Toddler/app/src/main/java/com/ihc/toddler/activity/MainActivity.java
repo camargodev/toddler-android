a4e3dfb33cde27ca7703ed5652f36a945f96346f
camargodev <camargodev@gmail.com>
2020-10-14 18:11:51 -0300

    Refactoring of card modules

--- Toddler/app/src/main/java/com/ihc/toddler/activity/MainActivity.java
+++ Toddler/app/src/main/java/com/ihc/toddler/activity/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.ihc.toddler.R;
+import com.ihc.toddler.adapter.ActivityCardAdapter;
 import com.ihc.toddler.entity.AbstractActivity;
 import com.ihc.toddler.repository.ContentRepository;
 import com.ihc.toddler.repository.QuizRepository;
@@ -17,7 +18,6 @@ import android.annotation.TargetApi;
 import android.os.Build;
 
 public class MainActivity extends AppCompatActivity {
-    private List<AbstractActivity> activities = new ArrayList<>();
     @TargetApi(Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+
+        List<AbstractActivity> activities = populateAcitivies();
         ActivityCardAdapter activityCardAdapter = new ActivityCardAdapter(activities);
+
         RecyclerView.LayoutManager manager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(activityCardAdapter);
-        StudentDataPrepare();
     }
 
-    private void StudentDataPrepare() {
+    private List<AbstractActivity>  populateAcitivies() {
+        List<AbstractActivity> activities = new ArrayList<>();
         AbstractActivity quiz = QuizRepository.getQuiz();
         AbstractActivity content = ContentRepository.getContent();
-        activities.add(quiz);
-        activities.add(content);
+        for (int i = 0; i < 7; i ++) {
+            activities.add(quiz);
+            activities.add(content);
+        }
+        return activities;
     }
 }
--- Toddler/app/src/main/java/com/ihc/toddler/adapter/ActivityCardAdapter.java
+++ Toddler/app/src/main/java/com/ihc/toddler/adapter/ActivityCardAdapter.java
@@ -1,4 +1,4 @@
-package com.ihc.toddler.activity;
+package com.ihc.toddler.adapter;
 
 import android.graphics.Color;
 import android.view.LayoutInflater;
@@ -12,13 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.ihc.toddler.R;
 import com.ihc.toddler.entity.AbstractActivity;
-import com.ihc.toddler.entity.Exercise;
 import com.ihc.toddler.entity.Quiz;
 
 import java.util.List;
 import java.util.Random;
 
-class ActivityCardAdapter extends RecyclerView.Adapter {
+public class ActivityCardAdapter extends RecyclerView.Adapter<ActivityCardAdapter.ActivityViewHolder> {
 
     List<AbstractActivity> activities;
 
@@ -28,21 +27,20 @@ class ActivityCardAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public ActivityViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int position) {
         View itemView = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.student_list_row, viewGroup, false);
-        return new MyViewHolder(itemView);
+        return new ActivityViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ActivityViewHolder holder, int position) {
         AbstractActivity activity = activities.get(position);
-        MyViewHolder myViewHolder = (MyViewHolder) holder;
         Random rnd = new Random();
         int currentColor = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-        myViewHolder.parent.setBackgroundColor(currentColor);
-        myViewHolder.name.setText(activity.getTitle());
-        myViewHolder.age.setText((activity instanceof Quiz) ? "Quiz" : "Conteudo");
+        holder.parent.setBackgroundColor(currentColor);
+        holder.name.setText(activity.getTitle());
+        holder.age.setText((activity instanceof Quiz) ? "Quiz" : "Conteudo");
 
     }
 
@@ -51,10 +49,10 @@ class ActivityCardAdapter extends RecyclerView.Adapter {
         return activities.size();
     }
 
-    static class MyViewHolder extends RecyclerView.ViewHolder {
+    static class ActivityViewHolder extends RecyclerView.ViewHolder {
         TextView name,age;
         LinearLayout parent;
-        public MyViewHolder(View itemView) {
+        public ActivityViewHolder(View itemView) {
             super(itemView);
             parent = itemView.findViewById(R.id.parent);
             name = itemView.findViewById(R.id.name);
