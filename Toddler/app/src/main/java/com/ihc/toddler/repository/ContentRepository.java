6fd8c55aaa37c3e7f40bbd2c0b39eba8f8997f8c
camargodev <camargodev@gmail.com>
2020-11-03 21:41:46 -0300

    Much more content and quizes on repo

--- Toddler/app/src/main/java/com/ihc/toddler/repository/ContentRepository.java
+++ Toddler/app/src/main/java/com/ihc/toddler/repository/ContentRepository.java
@@ -18,7 +18,11 @@ public class ContentRepository {
                 new Content(2, getWordTypeContent1()),
                 new Content(3, getWordTypeContent2()),
                 new Content(4, getWordTypeContent3()),
-                new Content(5, getWordTypeContent4()));
+                new Content(5, getWordTypeContent4()),
+                new Content(6, getVogalMeets()),
+                new Content(7, getSyllableType1()),
+                new Content(8, getSyllableType2()),
+                new Content(9, getSyllableType3()));
     }
 
     private static Content getSyllableSeparationContent() {
@@ -43,7 +47,7 @@ public class ContentRepository {
                         "Os tipos são: monossílaba, dissílaba, trissílaba e polissílaba."));
         parts.add(new ContentPart("Monossílaba" ,
                 "É uma palavra com apenas uma sílaba.\n\nExemplo: MAR\n"));
-        return new Content("Tipos de palavras 1", parts);
+        return new Content("Monossílabas", parts);
     }
 
     private static Content getWordTypeContent2() {
@@ -54,7 +58,7 @@ public class ContentRepository {
                         "Os tipos são: monossílaba, dissílaba, trissílaba e polissílaba."));
         parts.add(new ContentPart("Dissílaba" ,
                 "É uma palavra com duas sílabas.\n\nExemplo: FE-LIZ\n"));
-        return new Content("Tipos de palavras 2", parts);
+        return new Content("Dissílabas", parts);
     }
 
     private static Content getWordTypeContent3() {
@@ -65,7 +69,7 @@ public class ContentRepository {
                         "Os tipos são: monossílaba, dissílaba, trissílaba e polissílaba."));
         parts.add(new ContentPart("Trissílaba" ,
                 "É palavra com três sílabas.\n\nExemplo: BA-TA-TA\n"));
-        return new Content("Tipos de palavras 3", parts);
+        return new Content("Trissílabas", parts);
     }
 
     private static Content getWordTypeContent4() {
@@ -76,6 +80,62 @@ public class ContentRepository {
                         "Os tipos são: monossílaba, dissílaba, trissílaba e polissílaba."));
         parts.add(new ContentPart("Polissílaba" ,
                 "É uma palavra com 4 ou mais sílabas.\n\nExemplo: BRA-SI-LEI-RO"));
-        return new Content("Tipos de palavras 4", parts);
+        return new Content("Polissílabas", parts);
+    }
+
+    private static Content getVogalMeets() {
+        List<ContentPart> parts = new ArrayList<>();
+        parts.add(new ContentPart("O que é um encontro vocal?" ,
+                "É quando, em uma palavra, duas ou mais vogais se encontram.\n" +
+                        "Um encontro vocal pode ser um ditongo, um tritongo ou um hiato."));
+        parts.add(new ContentPart("Ditongo" ,
+                "É o encontro de duas vogais em uma só sílaba." +
+                        "\nExemplos: pai (ditongo = AI), mãe (ditongo = ÃE), cenoura (ditongo = OU)"));
+        parts.add(new ContentPart("Tritongo" ,
+                "É o encontro de três vogais em uma só sílaba." +
+                        "\nExemplos: Paraguai (tritongo = UAI), quão (trigonto= UÃO)"));
+        parts.add(new ContentPart("Hiato" ,
+                "É o encontro de duas vogais em sílabas diferentes." +
+                        "\nExemplos: saída (o A e o Í estão em sílabas diferentes)"));
+        return new Content("Encontros vocais", parts);
+    }
+
+    private static Content getSyllableType1() {
+        List<ContentPart> parts = new ArrayList<>();
+        parts.add(new ContentPart("O que é uma sílaba tônica?" ,
+                "É a sílaba cuja pronúncia é feita com " +
+                        "maior intensidade.\n" +
+                        "Sempre há apenas uma sílaba tônica por palavra."));
+        parts.add(new ContentPart("Exemplo" ,
+                "Na palavra BORBOLETA, as sílabas são: bor-bo-le-ta." +
+                        "\nA sílaba tônica é LE, pois é a que mais destacamos ao falar."));
+        return new Content("Sílabas Tônicas", parts);
+    }
+
+    private static Content getSyllableType2() {
+        List<ContentPart> parts = new ArrayList<>();
+        parts.add(new ContentPart("O que é uma sílaba átona?" ,
+                "São as sílabas cuja pronúncia é feita com " +
+                        "menor intensidade.\n" +
+                        "É o contrário da sílaba tônica. " +
+                        "Toda sílaba de uma palavra que não a tônica é átona."));
+        parts.add(new ContentPart("Exemplo" ,
+                "Na palavra BORBOLETA, as sílabas são: bor-bo-le-ta." +
+                        "\nAs sílabas tônica é BOR, BO e TA, pois LE é a sílaba tônica, " +
+                        "então todas as outras são átonas."));
+        return new Content("Sílabas Átonas", parts);
+    }
+
+    private static Content getSyllableType3() {
+        List<ContentPart> parts = new ArrayList<>();
+        parts.add(new ContentPart("Sílabas Tônicas e Acentos" ,
+                "Quando uma palavra possui acento grave (ˆ) ou agudo(´), " +
+                        "a sílaba que contém esse acento é sempre a tônica.\n" +
+                        "Cada palavra só pode ter um sílaba tônica " +
+                        "e portanto só pode ter um acento."));
+        parts.add(new ContentPart("Exemplo" ,
+                "Na palavra ÚLTIMO, as sílabas são: úl-ti-mo." +
+                        "\nA sílaba ÚL possui um acento, portanto é a tônica."));
+        return new Content("Tonalidade com Acentos", parts);
     }
 }
--- Toddler/app/src/main/java/com/ihc/toddler/repository/QuizRepository.java
+++ Toddler/app/src/main/java/com/ihc/toddler/repository/QuizRepository.java
@@ -12,6 +12,7 @@ import java.util.List;
 import static com.ihc.toddler.entity.MultipleChoiceExercise.A;
 import static com.ihc.toddler.entity.MultipleChoiceExercise.B;
 import static com.ihc.toddler.entity.MultipleChoiceExercise.C;
+import static com.ihc.toddler.entity.MultipleChoiceExercise.D;
 import static com.ihc.toddler.entity.TrueOrFalseExercise.TRUE;
 import static com.ihc.toddler.entity.TrueOrFalseExercise.FALSE;
 
@@ -20,14 +21,18 @@ public class QuizRepository {
     private static int id = 0;
 
     public static List<Quiz> getQuizes() {
-        return Arrays.asList(new Quiz(1, getSyllableSeparationQuiz()), new Quiz(2, getWordTypesQuiz()));
+        return Arrays.asList(new Quiz(1, getSyllableSeparationQuiz()),
+                             new Quiz(2, getWordTypesQuiz()),
+                             new Quiz(3, getSyllableType1()),
+                             new Quiz(4, getSyllableType2()),
+                             new Quiz(5, getSyllableType3()));
     }
 
     private static Quiz getSyllableSeparationQuiz() {
         List<Exercise> exercises = new ArrayList<>();
         exercises.add(new MultipleChoiceExercise("Quantas sílabas\ntem a palavra\ncamelo?", Arrays.asList("1", "2", "3", "4"), C));
         exercises.add(new TrueOrFalseExercise("As sílabas de PATO\nsão PA e TO?", TRUE));
-        exercises.add(new MultipleChoiceExercise("Quais são as\nsílabas de AMOR?", Arrays.asList("AM-OR", "AMOR", "A-MOR", "AMO-R"), A));
+        exercises.add(new MultipleChoiceExercise("Quais são as\nsílabas de BALÃO?", Arrays.asList("BA-LÃO", "BAL-ÃO", "BALÃ-O", "BA-LÃ-O"), A));
         return new Quiz("Separação de sílabas", exercises);
     }
 
@@ -37,6 +42,29 @@ public class QuizRepository {
         exercises.add(new MultipleChoiceExercise("Se uma palavra é\numa dissílaba, quantas\nsílabas ela tem?", Arrays.asList("1", "2", "3", "4"), B));
         exercises.add(new TrueOrFalseExercise("Uma palavra com 5\nsílabas é uma polissílaba?", TRUE));
         return new Quiz("Tipos de palavras", exercises);
+    }
+
+    private static Quiz getSyllableType1() {
+        List<Exercise> exercises = new ArrayList<>();
+        exercises.add(new MultipleChoiceExercise("Qual é a sílaba tônica\nda palavra CEBOLA?", Arrays.asList("CE", "BO", "BOL", "LA"), B));
+        exercises.add(new TrueOrFalseExercise("A palavra mar tem uma sílaba tônica", TRUE));
+        return new Quiz("Sílabas tônicas", exercises);
+    }
+
+    private static Quiz getSyllableType2() {
+        List<Exercise> exercises = new ArrayList<>();
+        exercises.add(new TrueOrFalseExercise("Toda palavra tem no mínimo\numa sílaba átona", FALSE));
+        exercises.add(new MultipleChoiceExercise("Quantas sílabas átonas\ntem a palavra CEBOLA?", Arrays.asList("0", "1", "2", "3"), C));
+        exercises.add(new TrueOrFalseExercise("A palavra sal tem uma sílaba átona", FALSE));
+        return new Quiz("Sílabas átonas", exercises);
+    }
+
+    private static Quiz getSyllableType3() {
+        List<Exercise> exercises = new ArrayList<>();
+        exercises.add(new TrueOrFalseExercise("A sílaba tônica de CÁLICE É LI", FALSE));
+        exercises.add(new MultipleChoiceExercise("Quantas são as sílabas átonas\nda palavra PARANÁ?", Arrays.asList("NÁ", "PA E NÁ", "RA e NÁ", "PA e RA"), D));
+        exercises.add(new TrueOrFalseExercise("A sílaba átona de JILÓ tem é JI", TRUE));
+        return new Quiz("Tonalidade com Acentos", exercises);
     }
 
 }
