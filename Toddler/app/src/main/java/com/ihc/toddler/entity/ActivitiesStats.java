baec53011284e67dfc3b9e7342a09f57d7f1c4f1
camargodev <camargodev@gmail.com>
2020-11-04 21:08:58 -0300

    Activity stats

---
+++ Toddler/app/src/main/java/com/ihc/toddler/entity/ActivitiesStats.java
@@ -0,0 +1,20 @@
+package com.ihc.toddler.entity;
+
+public class ActivitiesStats {
+
+    int totalNumberOfActivities;
+    int numberOfConsumesActivities;
+
+    public ActivitiesStats(int totalNumberOfActivities, int numberOfConsumesActivities) {
+        this.totalNumberOfActivities = totalNumberOfActivities;
+        this.numberOfConsumesActivities = numberOfConsumesActivities;
+    }
+
+    public int getTotalNumberOfActivities() {
+        return totalNumberOfActivities;
+    }
+
+    public int getNumberOfConsumesActivities() {
+        return numberOfConsumesActivities;
+    }
+}
--- Toddler/app/src/main/java/com/ihc/toddler/persistence/ActivityDataConsumer.java
+++ Toddler/app/src/main/java/com/ihc/toddler/persistence/ActivityDataConsumer.java
@@ -1,12 +1,18 @@
 package com.ihc.toddler.persistence;
 
+import com.ihc.toddler.entity.ActivitiesStats;
 import com.ihc.toddler.entity.Content;
 import com.ihc.toddler.entity.Quiz;
+import com.ihc.toddler.repository.ContentRepository;
+import com.ihc.toddler.repository.QuizRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ActivityDataConsumer extends ActivityTracker {
+
+    private static final int totalNumberOfContents = ContentRepository.getContents().size();
+    private static final int totalNumberOfQuizes = QuizRepository.getQuizes().size();
 
     public static List<Content> getAllConsumedContents() {
         return new ArrayList<>(contents.values());
@@ -14,6 +20,14 @@ public class ActivityDataConsumer extends ActivityTracker {
 
     public static List<Quiz> getAllConsumedQuizes() {
         return new ArrayList<>(quizes.values());
+    }
+
+    public static ActivitiesStats getContentStats() {
+        return new ActivitiesStats(totalNumberOfContents, contents.size());
+    }
+
+    public static ActivitiesStats getQuizStats() {
+        return new ActivitiesStats(totalNumberOfQuizes, quizes.size());
     }
 
 }
